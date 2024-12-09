02fbe46b2e943da46a530a4ec192e9ae04a43639
winters0x64 <arun0x168codes@gmail.com>
2024-12-09 21:28:53 +0530

    Finished the basic blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,11 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), preact()],
+  site: "http://localhost:4321"
+
 });
