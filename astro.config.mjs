8994dbbb6ee01a38c2ac59a3fd7300a9bc168914
winters0x64 <arun0x168codes@gmail.com>
2024-12-18 21:58:36 +0530

    Fix improper image

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,5 +9,5 @@ import preact from '@astrojs/preact';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact()],
-  site: "https://winters0x64.github.io",
+  site: "https://winters0x64.xyz",
 });
